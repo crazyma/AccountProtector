3528dd5a2729f838f520459e418a40b1d7ecdc4d
David-Retina15 <david77115@gmail.com>
2017-06-15 18:39:08 +0800

    add back arrow button

--- app/src/main/java/com/beibeilab/accountprotector/feature/account/AddAccountActivity.java
+++ app/src/main/java/com/beibeilab/accountprotector/feature/account/AddAccountActivity.java
@@ -2,6 +2,8 @@ package com.beibeilab.accountprotector.feature.account;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.beibeilab.accountprotector.R;
 
@@ -11,5 +13,21 @@ public class AddAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_account);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
