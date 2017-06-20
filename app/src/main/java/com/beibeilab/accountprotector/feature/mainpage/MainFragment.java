05e9ca9a2a66693e13e19abb734fd3cb14c8cb70
David-Retina15 <david77115@gmail.com>
2017-06-20 22:15:39 +0800

    use LiveData to load db in MainFragment

--- app/src/main/java/com/beibeilab/accountprotector/feature/mainpage/MainFragment.java
+++ app/src/main/java/com/beibeilab/accountprotector/feature/mainpage/MainFragment.java
@@ -1,8 +1,10 @@
 package com.beibeilab.accountprotector.feature.mainpage;
 
+import android.arch.lifecycle.LifecycleFragment;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.databinding.DataBindingUtil;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -15,30 +17,18 @@ import com.beibeilab.accountprotector.databinding.MainFragmentBinding;
 import com.beibeilab.accountprotector.room.AccountDatabase;
 import com.beibeilab.accountprotector.room.AccountEntity;
 
-import org.reactivestreams.Publisher;
-
 import java.util.ArrayList;
 import java.util.List;
-
-import io.reactivex.Flowable;
-import io.reactivex.SingleObserver;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
-import io.reactivex.observers.DisposableSingleObserver;
-import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subscribers.DisposableSubscriber;
-import timber.log.Timber;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends LifecycleFragment implements Runnable{
 
     private MainFragmentBinding mBinding;
     private MainListViewModel viewModel;
     private AccountDatabase accountDatabase;
+    private LiveData<List<AccountEntity>> liveData;
 
     private RecyclerView mRecyclerView;
 
@@ -70,102 +60,41 @@ public class MainFragment extends Fragment {
         setupRecyclerView();
     }
 
-    private void setupRecyclerView(){
-        accountDatabase = AccountDatabase.getInstance(getContext());
-//        accountDatabase.getAccountDao().getAllFlowable()
-//                .subscribeOn(Schedulers.io())
-//                .flatMap(new Function<List<AccountEntity>, Publisher<AccountEntity>>() {
-//                    @Override
-//                    public Publisher<AccountEntity> apply(List<AccountEntity> accountEntities) throws Exception {
-//                        return Flowable.fromIterable(accountEntities);
-//                    }
-//                })
-//                .map(new Function<AccountEntity, MainItemViewModel>() {
-//                    @Override
-//                    public MainItemViewModel apply(AccountEntity accountEntity) throws Exception {
-//                        MainItemViewModel viewModel = new MainItemViewModel(accountEntity);
-//                        return viewModel;
-//                    }
-//                })
-//                .doOnNext(new Consumer<MainItemViewModel>() {
-//                    @Override
-//                    public void accept(MainItemViewModel mainItemViewModel) throws Exception {
-//                        Timber.d("view model title : " + mainItemViewModel.getTextName());
-//                    }
-//                })
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribeWith(new DisposableSubscriber<MainItemViewModel>() {
-//                    @Override
-//                    public void onNext(MainItemViewModel mainItemViewModel) {
-//                        Timber.d("view model : " + mainItemViewModel.getTextName());
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable t) {
-//                        Timber.e(t.toString());
-//                    }
-//
-//                    @Override
-//                    public void onComplete() {
-//                        Timber.d("on complete");
-//                    }
-//                });
-//                .toList()
-//                .doOnSuccess(new Consumer<List<MainItemViewModel>>() {
-//                    @Override
-//                    public void accept(List<MainItemViewModel> mainItemViewModels) throws Exception {
-//                        Timber.d("XDDDDDDDD " + mainItemViewModels.size());
-//                    }
-//                })
-//                .doOnError(new Consumer<Throwable>() {
-//                    @Override
-//                    public void accept(Throwable throwable) throws Exception {
-//                        Timber.e(throwable.toString());
-//                    }
-//                })
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribeWith(new DisposableSingleObserver<List<MainItemViewModel>>() {
-//                    @Override
-//                    public void onSuccess(List<MainItemViewModel> mainItemViewModels) {
-//                        Timber.d("AAAAAAAAAAAAAAAAAAA");
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//                        Timber.e("EEEEEEEEEEEEEEEEEEEEE");
-//                    }
-//                });
-//                .subscribeWith(new DisposableSingleObserver<List<MainItemViewModel>>() {
-//                    @Override
-//                    public void onSuccess(List<MainItemViewModel> mainItemViewModels) {
-//                        List<MainItemViewModel> mainItemViewModelList = viewModel.getMainItemViewModelList();
-//                        mainItemViewModelList.clear();
-//                        mainItemViewModelList.addAll(mainItemViewModelList);
-//                        mRecyclerView.getAdapter().notifyDataSetChanged();
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//                        Timber.e(e.toString());
-//                    }
-//                });
-//                .subscribeWith(new DisposableSubscriber<AccountEntity>() {
-//                    @Override
-//                    public void onNext(AccountEntity accountEntity) {
-//                        Timber.d("result : " + accountEntity);
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable t) {
-//                        Timber.e(t.toString());
-//                    }
-//
-//                    @Override
-//                    public void onComplete() {
-//
-//                    }
-//                });
+    @Override
+    public void run() {
+        liveData.observe(MainFragment.this, new Observer<List<AccountEntity>>() {
+            @Override
+            public void onChanged(@Nullable List<AccountEntity> accountEntities) {
+                List<MainItemViewModel> mainItemViewModelList = viewModel.getMainItemViewModelList();
+                mainItemViewModelList.clear();
 
+                for (AccountEntity accountEntity : accountEntities) {
+                    mainItemViewModelList.add(new MainItemViewModel(accountEntity));
+                }
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mRecyclerView.getAdapter().notifyDataSetChanged();
+                    }
+                });
+            }
+        });
+    }
+
+    private void setupRecyclerView(){
+        setupRecyclerViewWithLiveData();
+//        setupRecyclerViewWithSingleDaoInThread();
+    }
+
+    private void setupRecyclerViewWithLiveData(){
+        accountDatabase = AccountDatabase.getInstance(getContext());
+        liveData = accountDatabase.getAccountDao().getAllFromLiveData();
+        new Thread(this).start();
+    }
+
+    private void setupRecyclerViewWithSingleDaoInThread(){
+        accountDatabase = AccountDatabase.getInstance(getContext());
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -192,4 +121,5 @@ public class MainFragment extends Fragment {
             }
         }).start();
     }
+
 }
--- app/src/main/java/com/beibeilab/accountprotector/room/AccountDao.java
+++ app/src/main/java/com/beibeilab/accountprotector/room/AccountDao.java
@@ -1,5 +1,6 @@
 package com.beibeilab.accountprotector.room;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
@@ -23,6 +24,9 @@ public interface AccountDao {
 
     @Query("SELECT * FROM accountEntity")
     Flowable<List<AccountEntity>> getAllFlowable();
+
+    @Query("SELECT * FROM accountEntity")
+    LiveData<List<AccountEntity>> getAllFromLiveData();
 
     @Insert
     void insert(AccountEntity accountEntity);
