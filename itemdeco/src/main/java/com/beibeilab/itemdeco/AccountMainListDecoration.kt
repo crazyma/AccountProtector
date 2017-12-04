e5d18b5cbcc64a66dccb03436a45430b60c413cb
David-Retina15 <david77115@gmail.com>
2017-12-04 20:14:58 +0800

    feat: create AccountMainListDecoration, inherited from BaseItemDecoration

---
+++ itemdeco/src/main/java/com/beibeilab/itemdeco/AccountMainListDecoration.kt
@@ -0,0 +1,24 @@
+package com.beibeilab.itemdeco
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.Log
+import com.beibeilab.itemdeco.TransferDecoration.Companion.ATRRS
+
+/**
+ * Created by david on 2017/12/4.
+ */
+class AccountMainListDecoration(context: Context) : BaseItemDecoration() {
+
+    init {
+        val ta: TypedArray = context.obtainStyledAttributes(ATRRS)
+        divider = ta.getDrawable(0)
+        ta.recycle()
+
+        drawablePaddingLeft = context.resources.getDimensionPixelSize(R.dimen.main_list_text_padding_left)
+
+        paddingOnBottom = true
+        Log.d("crazyma", "init in Child")
+        Log.d("crazyma", "interval is $interval")
+    }
+}
--- itemdeco/src/main/java/com/beibeilab/itemdeco/BaseItemDecoration.kt
+++ itemdeco/src/main/java/com/beibeilab/itemdeco/BaseItemDecoration.kt
@@ -1,36 +1,41 @@
 package com.beibeilab.itemdeco
 
-import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
 
 /**
  * Created by david on 2017/12/1.
  */
 
-abstract class BaseItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
+abstract class BaseItemDecoration : RecyclerView.ItemDecoration() {
     protected var paddingLeft: Int
     protected var paddingTop: Int
     protected var paddingRight: Int
     protected var paddingBottom: Int
+    protected var drawablePaddingLeft: Int
+    protected var drawablePaddingTop: Int
+    protected var drawablePaddingRight: Int
+    protected var drawablePaddingBottom: Int
     protected var interval: Int
     protected var orientation: Int
     protected var paddingOnTop: Boolean
     protected var paddingOnBottom: Boolean
 
-    private var divider: Drawable? = null
+    protected var divider: Drawable? = null
         set(value) {
             if (value != null) {
+                Log.d("crazyma","!!!!!!!!!!!!!!!")
                 interval = when (orientation) {
                     HORIZONTAL_LIST -> value.intrinsicWidth
                     else -> value.intrinsicHeight
                 }
+                field = value
             }
-
         }
 
     init {
@@ -39,9 +44,14 @@ abstract class BaseItemDecoration(context: Context) : RecyclerView.ItemDecoratio
         paddingRight = 0
         paddingBottom = 0
         interval = 0
+        drawablePaddingLeft = 0
+        drawablePaddingTop = 0
+        drawablePaddingRight = 0
+        drawablePaddingBottom = 0
         paddingOnTop = false
         paddingOnBottom = false
         orientation = VERTICAL_LIST
+        Log.d("crazyma", "init in Base")
     }
 
     override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
@@ -51,8 +61,8 @@ abstract class BaseItemDecoration(context: Context) : RecyclerView.ItemDecoratio
 
                 }
                 else -> {
-                    val left = parent.paddingLeft - paddingRight
-                    val right = parent.width - parent.paddingRight - paddingRight
+                    val left = parent.paddingLeft + drawablePaddingLeft
+                    val right = parent.width - parent.paddingRight - drawablePaddingRight
 
                     val childCount = parent.childCount
                     for (i in 0 until childCount) {
@@ -60,7 +70,7 @@ abstract class BaseItemDecoration(context: Context) : RecyclerView.ItemDecoratio
                         val params = child.layoutParams as RecyclerView.LayoutParams
 
                         val top =
-                                if (paddingOnTop) interval
+                                if (paddingOnTop) interval + child.bottom + params.bottomMargin
                                 else child.bottom + params.bottomMargin
 
 
